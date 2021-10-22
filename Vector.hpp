c321ef786a7751ffc83bee147af90c176c1dae97
Loic Eickmayer <leickmay@z1r11p3.42lyon.fr>
2021-10-22 10:53:29 +0200

    ...

--- Vector.hpp
+++ Vector.hpp
@@ -11,6 +11,14 @@ namespace ft
 		public:
 			typedef T		value_type;
 			typedef Alloc	allocator_type;
+
+			typedef typename std::size_t	size_type;
+			typedef typename std::ptrdiff_t	difference_type;
+
+			typedef typename allocator_type::reference			reference;
+			typedef typename allocator_type::const_reference	const_reference;
+			typedef typename allocator_type::pointer			pointer;
+			typedef typename allocator_type::const_pointer		const_pointer;
 //default (1)	
 			explicit vector (const allocator_type& alloc = allocator_type()){};
 //fill (2)	
